3647fe50a555b237a5e496a5f49e164a531fc08d
korrnel <korrnel@gmail.com>
2017-05-10 22:43:28 +0200

    Soution 2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,16 +1,76 @@
 (function () {
 'use strict';
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
+angular.module('ShoppingListApp', [])
+.controller('ShoppingListShowController', ShoppingListShowController)
+.controller('BoughtListShowController', BoughtListShowController)
+.service('ShoppingListService', ShoppingListService);
 
-function DIController ($scope,$filter) {
-  $scope.name = "Yaakov";
 
-  $scope.upper=function(){
-    var upcase =$filter('uppercase');
-    $scope.name=upcase($scope.name);
+// ShoppingListShowController.$inject = ['ShoppingListService'];
+function ShoppingListShowController(ShoppingListService) {
+  var itemAdder = this;
+
+  itemAdder.items  =[{
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
+  }
+];
+
+
+  itemAdder.BuyItem = function (itemIndex) {
+    ShoppingListService.addItem(itemAdder.items[itemIndex].name,itemAdder.items[itemIndex].quantity);
+    itemAdder.items.splice(itemIndex, 1);
   }
 }
+
+// BoughtListShowController.$inject = ['ShoppingListService'];
+function BoughtListShowController(ShoppingListService) {
+  var BoughtList = this;
+
+  BoughtList.items=ShoppingListService.getItems();
+
+
+}
+
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
+
+  };
+
+  service.removeItem = function (itemIndex) {
+
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
 
 })();
